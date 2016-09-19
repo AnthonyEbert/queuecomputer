ac723b1e1bff8992589ab3e449c0ae68c2e644f6
AnthonyEbert <anthonyebert@gmail.com>
2016-09-19 19:50:13 +1000

    preparing help file information using roxygen2

--- R/core_queuefunctions.R
+++ R/core_queuefunctions.R
@@ -29,9 +29,23 @@ n_max <- function(input,n){
 #' @examples
 #' arrival_df <- data.frame(ID = c(1:100), times = rlnorm(100, meanlog = 3))
 #' service <- rlnorm(100)
-#' queue_step(arrival_df = arrival_df, service = service)
-#' queue_step(arrival_df = arrival_df,
+#' firstqueue <- queue_step(arrival_df = arrival_df, service = service)
+#' secondqueue <- queue_step(arrival_df = arrival_df,
 #'     Number_of_queues = stepfun(c(15,30,50), c(1,3,1,10)), service = service)
+#'
+#' curve(ecdf(arrival_df$times)(x) * 100 , from = 0, to = 200,
+#'     xlab = "time", ylab = "Number of customers")
+#' curve(ecdf(firstqueue$times)(x) * 100 , add = TRUE, col = "red")
+#' curve(ecdf(secondqueue$times)(x) * 100, add = TRUE, col = "blue")
+#' legend(100,40, legend = c("Customer input - arrivals",
+#'     "Customer output - firstqueue",
+#'     "Customer output - secondqueue"),
+#'     col = c("black","red","blue"), lwd = 1, cex = 0.8
+#' )
+#'
+#' # Queue lengths
+#' ecdf(arrival_df$times)(c(1:200))*100 - ecdf(firstqueue$times)(c(1:200))*100
+#' ecdf(arrival_df$times)(c(1:200))*100 - ecdf(secondqueue$times)(c(1:200))*100
 #'
 #' ord <- order(arrival_df$times)
 #' cbind(arrival_df[ord,], service[ord],
