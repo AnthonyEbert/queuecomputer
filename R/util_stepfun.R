f4dade03ff6c951cbd453d72ad1fe7c9615c3cf6
Anthony Ebert <anthonyebert@gmail.com>
2018-08-26 15:41:26 +1000

    Fixup utilisation functions.

--- R/util_stepfun.R
+++ R/util_stepfun.R
@@ -2,38 +2,54 @@
 # Utilization step function
 
 
-#' Performance dataframes
-performance_df <- function(arrivals, service = 0, departures, epsilon = 1e-10, servers){
+#' Performance data
+#' @export
+#' @param arrivals arrivals
+#' @param service service
+#' @param departures departures
+#' @param servers server object
+#' @param epsilon epsilon
+#' @examples
+#' n <- 300
+#' arrivals <- cumsum(rexp(n, 2.8))
+#' service <- rexp(n)
+#' x <- c(10, 40, 100)
+#' y <- c(1, 5, 2, 5)
+#' queue_obj <- queue_step(arrivals, service, servers = as.server.stepfun(x,y))
+#'
+#' output <- performance_output(arrivals, service, queue_obj$departures, queue_obj$servers_input)
+#'
+#' plot(output$busy_df, type = "l")
+#' plot(output$util_df, type = "l")
+performance_output <- function(arrivals, service = 0, departures, servers, epsilon = 1e-10 ){
 
   systemlength_df <- queue_lengths(arrivals, service = 0, departures, epsilon)
   #queuelength_df <- queue_lengths(arrivals, service, departures, epsilon)
 
   if("server.stepfun" %in% class(servers)){
-    servers <- stepfun(servers$x, servers$y)
+    servers <- stats::stepfun(servers$x, servers$y)
   }
 
   if(is.numeric(servers)){
     stopifnot(length(servers) == 1)
-    servers <- stepfun(1, c(servers, servers))
+    servers <- stats::stepfun(1, c(servers, servers))
   }
 
   output <- busy_util_stepfun(systemlength_df, servers, return_stepfun = FALSE, shorten = FALSE)
-  #output[["systemlength_df"]] <- systemlength_df
-  #output[["queuelength_df"]] <- queuelength_df
 
   return(output)
 }
 
 #' @export
 p_busy <- function(arrivals, service, departures, epsilon = 1e-10, servers){
-  output <- performance_df(arrivals, service, departures, epsilon, servers)
+  output <- performance_output(arrivals, service, departures, epsilon, servers)
 
   return(output$busy_df)
 }
 
 #' @export
 p_util <- function(arrivals, service, departures, epsilon = 1e-10, servers){
-  output <- performance_df(arrivals, service, departures, epsilon, servers)
+  output <- performance_output(arrivals, service, departures, epsilon, servers)
 
   return(output$util_df)
 }
@@ -44,7 +60,6 @@ p_util <- function(arrivals, service, departures, epsilon = 1e-10, servers){
 #' @param servers server stepfunction
 #' @param return_stepfun return performance measures as stepfunction or alternatively as dataframes
 #' @param shorten should the stepfunctions be shortened? Conceptually this should always be true but we're having problems with it at the moment.
-#' @export
 #' @examples
 #' n <- 100
 #' arrivals <- cumsum(rexp(n, 2.5))
@@ -53,15 +68,15 @@ p_util <- function(arrivals, service, departures, epsilon = 1e-10, servers){
 #' y <- c(1, 2, 1)
 #' queue_obj <- queue_step(arrivals, service, servers = as.server.stepfun(x,y))
 #'
-#' output <- busy_server_stepfun(queue_obj$systemlength_df, stepfun(x,y))
+#' #output <- busy_util_stepfun(queue_obj$systemlength_df, stats::stepfun(x,y))
 #'
-#' plot(output$busy_stepfun)
-#' plot(output$util_stepfun)
+#' #plot(output$busy_stepfun, type = "l")
+#' #plot(output$util_stepfun, type = "l")
 busy_util_stepfun <- function(systemlength_df, servers, return_stepfun = TRUE, shorten = TRUE){
 
-  sys_length_stepfun <- stepfun(systemlength_df$times[-1], systemlength_df$value)
+  sys_length_stepfun <- stats::stepfun(systemlength_df$times[-1], systemlength_df$value)
 
-  x_new <- unique(sort(c(knots(sys_length_stepfun), knots(servers))))
+  x_new <- unique(sort(c(stats::knots(sys_length_stepfun), stats::knots(servers))))
   y_new_busy <- rep(NA, length(x_new))
   y_new_util <- rep(NA, length(x_new))
 
@@ -93,8 +108,8 @@ busy_util_stepfun <- function(systemlength_df, servers, return_stepfun = TRUE, s
   if(return_stepfun == TRUE){
 
     output <- list(
-      busy_stepfun = stepfun(x_new_busy, c(0, y_new_busy)),
-      util_stepfun = stepfun(x_new_util, c(0, y_new_util))
+      busy_stepfun = stats::stepfun(x_new_busy, c(0, y_new_busy)),
+      util_stepfun = stats::stepfun(x_new_util, c(0, y_new_util))
     )
 
   } else {
